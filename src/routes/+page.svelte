776836918deeaa0f4564ef315180e4687486571c
kangruixiang <kangruixiang@gmail.com>
2026-07-12 16:16:47 -0400

    heat map works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,79 +1,11 @@
 <script lang="ts">
 	import { Computer, Mail } from '@lucide/svelte';
-	import { SvelteMap } from 'svelte/reactivity';
 
-	import { monthlyHeatmap } from '$lib/monthlyHeatmap';
-	import { FileHandlerState, type ScheduleRow } from './utils.svelte';
+	import { FileHandlerState } from './utils.svelte';
 
-	let selectedStaff = $state('');
-	let personRows = $state<ScheduleRow[]>([]);
-	let taskCounts: Record<string, number> = $state({});
+	const colors = ['heatmap-0', 'heatmap-1', 'heatmap-2', 'heatmap-3', 'heatmap-4'];
+
 	const data = new FileHandlerState();
-
-	const counts = new SvelteMap<string, number>();
-	let heatmapData = $derived.by(() => {
-		return Array.from(counts.entries()).map(([key, count]) => {
-			const [task, month] = key.split('|');
-
-			return {
-				task,
-				month: new Date(`${month}-01`),
-				count
-			};
-		});
-	});
-
-	let taskData = $derived.by(() => {
-		// converts object to array of arrays and then map to array of objects and sort by count descending
-		const data = Object.entries(taskCounts)
-			.map(([task, count]) => ({
-				task,
-				count
-			}))
-			.sort((a, b) => b.count - a.count);
-
-		const total = data.reduce((sum, item) => sum + item.count, 0);
-
-		return data.map((item) => ({
-			...item,
-			percent: (item.count / total) * 100
-		}));
-	});
-
-	let maxCount = $derived(Math.max(...taskData.map((d) => d.count)));
-
-	function getTaskCounts() {
-		taskCounts = {};
-		counts.clear();
-
-		// gets all rows for selected staff
-		personRows = data.rows.filter((row: any) => row['Staff Last Name'] === selectedStaff);
-
-		// gets task counts for selected staff
-		for (const row of personRows) {
-			const originalTask = row['Task Abbreviation'].replace(/-$/, '').trim();
-
-			const task = originalTask.replace(/\s*\(?(AM|PM)\)?/, '');
-			const isHalfDay = task !== originalTask;
-
-			taskCounts[task] ??= 0;
-
-			taskCounts[task] += isHalfDay ? 0.5 : 1;
-
-			// count the number of times for each month
-			const date = new Date(row['Schedule Date']);
-
-			const month = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
-
-			const key = `${task}|${month}`;
-
-			if (!counts.has(key)) {
-				counts.set(key, 1);
-			} else {
-				counts.set(key, counts.get(key)! + 1);
-			}
-		}
-	}
 </script>
 
 <h1 class="pb-3 text-primary font-semibold">Qgenda Plus</h1>
@@ -82,7 +14,7 @@
 <div class="flex flex-col gap-y-4">
 	<input type="file" accept=".csv" onchange={data.handleFile} />
 
-	<select bind:value={selectedStaff} class="select" onchange={getTaskCounts}>
+	<select bind:value={data.selectedStaff} class="select" onchange={data.getTaskCounts}>
 		<option disabled selected>Name</option>
 		{#if data.staffNames.length > 0}
 			{#each data.staffNames as name (name)}
@@ -93,21 +25,21 @@
 
 	<hr />
 
-	<h2>{selectedStaff}</h2>
+	<h2>{data.selectedStaff}</h2>
 
-	{#if selectedStaff}
-		<!-- chart:
-		<PieChart data={taskData} key="task" value="count" height={300} cRange={schemeTableau10} /> -->
-
+	{#if data.selectedStaff}
 		<div class="space-y-3">
-			{#each taskData as item, i (item)}
+			{#each data.taskObject as item, i (item)}
 				<div class="grid grid-cols-12 gap-y-golden-sm items-center hover:bg-base-200 px-golden-md">
 					<div class="col-span-3 my-golden-sm">
 						{item.task}
 					</div>
 
 					<div class="bg-base-300 h-6 col-span-8 border border-black">
-						<div class="bg-primary h-6" style={`width: ${(item.count / maxCount) * 100}%`}></div>
+						<div
+							class="bg-primary h-6"
+							style={`width: ${(item.count / data.totalCount) * 100}%`}
+						></div>
 					</div>
 
 					<div class="col-span-1 text-right">
@@ -118,11 +50,36 @@
 			{/each}
 		</div>
 
-		{#each heatmapData as item, i (item)}
-			{item.task} - {item.count}
-		{/each}
+		<div
+			class="grid gap-golden-md grid-cols-12 overflow-x-auto"
+			style={`grid-template-columns: 12rem repeat(${data.months.length}, 30px)`}
+		>
+			<div></div>
+			{#each data.months as month (month)}
+				<div class="text-center w-full">
+					{new Date(`${month}-01`).toLocaleDateString(undefined, { month: 'short' })}
+				</div>
+			{/each}
 
-		<div use:monthlyHeatmap={heatmapData}></div>
+			{#each data.taskObject as item (item)}
+				<div>
+					{item.task}
+				</div>
+
+				{#each data.months as month (month)}
+					{@const count = data.counts.get(`${item.task}|${month}`) ?? 0}
+
+					{@const level = data.getLevel(item.task, count)}
+
+					<div
+						class={`h-8 w-8 text-center rounded-sm flex items-center justify-center justify-self-center ${colors[level]}`}
+						title={`${item.task}: ${count}`}
+					>
+						{count}
+					</div>
+				{/each}
+			{/each}
+		</div>
 	{/if}
 </div>
 
--- src/routes/utils.svelte.ts
+++ src/routes/utils.svelte.ts
@@ -1,5 +1,5 @@
 import Papa from 'papaparse';
-import { SvelteSet } from 'svelte/reactivity';
+import { SvelteSet, SvelteMap, SvelteDate } from 'svelte/reactivity';
 
 export type ScheduleRow = {
 	'Schedule Date': string;
@@ -10,8 +10,20 @@ export type ScheduleRow = {
 };
 
 export class FileHandlerState {
-	rows: ScheduleRow[] = $state([]);
+	rows = $state<ScheduleRow[]>([]);
+	personRows = $state<ScheduleRow[]>([]);
+
 	staffNames = $state<string[]>([]);
+	selectedStaff = $state('');
+
+	months = $state<string[]>([]);
+	counts = new SvelteMap<string, number>([]);
+	totalCount = $state(0);
+	taskTotals = new SvelteMap<string, number>([]);
+
+	taskCounts: Record<string, number> = $state({});
+	taskObject = $state<{ task: string; count: number; percent: number }[]>([]);
+	heatmapData = $state<{ task: string; month: Date; count: number }[]>([]);
 
 	handleFile = (event: Event) => {
 		const file = (event.target as HTMLInputElement).files?.[0];
@@ -32,4 +44,130 @@ export class FileHandlerState {
 			}
 		});
 	};
+
+	/**  
+	// gets rows for selected staff and counts the tasks as well as total for each month
+	*/
+	getTaskCounts = () => {
+		this.taskCounts = {};
+		if (this.counts) this.counts.clear();
+
+		// gets all rows for selected staff
+		this.personRows = this.rows.filter((row: any) => row['Staff Last Name'] === this.selectedStaff);
+
+		// gets task counts for selected staff
+		for (const row of this.personRows) {
+			// for room 9 procedures with extra -
+			const originalTask = row['Task Abbreviation'].replace(/-$/, '').trim();
+
+			const task = originalTask.replace(/\s*\(?(AM|PM)\)?/, '');
+			const isHalfDay = task !== originalTask;
+
+			// initiates task to 0 otherwise gets undefined
+			this.taskCounts[task] ??= 0;
+
+			// count days and half days
+			this.taskCounts[task] += isHalfDay ? 0.5 : 1;
+
+			// total the task count for each month
+			this.getMonthlyCount(row['Schedule Date'], task, isHalfDay);
+		}
+
+		this.getTaskTotals(); // gets task totals for all year
+		this.getTaskObject(); // gets the array of tasks with counts for display
+		this.getTotalCount(); // gets total count for all tasks for person
+		this.getMonthsBetween(); // gets the months for heatmap
+	};
+
+	getMonthlyCount = (inputDate: string, task: string, isHalfDay: boolean) => {
+		const date = new SvelteDate(inputDate);
+
+		const month = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
+
+		const key = `${task}|${month}`;
+
+		if (!this.counts.has(key)) {
+			this.counts.set(key, isHalfDay ? 0.5 : 1);
+		} else {
+			this.counts.set(key, this.counts.get(key)! + (isHalfDay ? 0.5 : 1));
+		}
+	};
+
+	/**  
+	// total counts for individual task for person
+	*/
+	getTaskTotals = () => {
+		for (const [key, count] of this.counts) {
+			const [task] = key.split('|');
+
+			this.taskTotals.set(task, (this.taskTotals.get(task) ?? 0) + count);
+		}
+	};
+
+	/**  
+	// calculate total count for selected staff, including all of the tasks and all of the months. Used to calculate percentage
+	*/
+	getTotalCount = () => {
+		this.totalCount = Math.max(...this.taskObject.map((d) => d.count));
+	};
+
+	/**  
+	// gets task data for selected staff.  
+	*/
+	getTaskObject = () => {
+		// converts object to array of arrays and then map to array of objects and sort by count descending
+		const data = Object.entries(this.taskCounts)
+			.map(([task, count]) => ({
+				task,
+				count
+			}))
+			.sort((a, b) => b.count - a.count);
+
+		const total = data.reduce((sum, item) => sum + item.count, 0);
+
+		this.taskObject = data.map((item) => ({
+			...item,
+			percent: (item.count / total) * 100
+		}));
+	};
+
+	/**  
+	// gets all of the unique months  
+	*/
+	getMonthsBetween = () => {
+		const dates = this.rows.map((row) => new SvelteDate(row['Schedule Date']));
+
+		const minDate = new SvelteDate(Math.min(...dates.map((d) => d.getTime())));
+
+		const maxDate = new SvelteDate(Math.max(...dates.map((d) => d.getTime())));
+
+		const current = new SvelteDate(minDate.getFullYear(), minDate.getMonth(), 1);
+
+		const monthsWithDuplicates = [];
+
+		while (current <= maxDate) {
+			monthsWithDuplicates.push(
+				`${current.getFullYear()}-${String(current.getMonth() + 1).padStart(2, '0')}`
+			);
+
+			current.setMonth(current.getMonth() + 1);
+		}
+
+		this.months = [...new SvelteSet(monthsWithDuplicates)].sort();
+	};
+
+	/**  
+	// gets level of intensity for heatmap  
+	*/
+	getLevel(task: string, count: number) {
+		if (count === 0) return 0;
+
+		const intensity = count / (this.taskTotals.get(task) ?? 1);
+
+		if (intensity < 0.1) return 1;
+		if (intensity < 0.2) return 2;
+		if (intensity < 0.35) return 3;
+
+		return 4;
+	}
 }
